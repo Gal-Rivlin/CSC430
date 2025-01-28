8379386ea0b420a67adfe8633b560d605806f472
Gal-Rivlin <grivlin@calpoly.edu>
2025-01-28 14:29:14 -0800

    empty racket file

--- asgn3.rkt
+++ asgn3.rkt
@@ -35,12 +35,16 @@
 (define (parse [exp : Sexp]) : ExprC
   (match exp
     [(? real? n) (NumC n)]
-    [(list (? symbol? n) left right) (BinOp n (parse left) (parse right))]
+    [(list (? is-op? n) left right) (BinOp (cast n Symbol) (parse left) (parse right))]
     [(? symbol? s) (Id s)]
     [(list 'ifleq0? val then else) (IfLeq0? (parse val) (parse then) (parse else))]
     [(list (? symbol? s) x ...) (Fcall (Id s) (map parse x))]
     [other (error 'parse "QWJZ - expected valid expression, got ~e" other)]))
 
+;; is-op? checks if an sexp is an op
+
+(define (is-op? [exp : Sexp]) : Boolean
+  (or (equal? '+ exp) (equal? '- exp) (equal? '/ exp) (equal? '* exp)))
 
 ;; parse-fundef parses an s-exp into a funcion def
 
@@ -65,19 +69,10 @@
   (match a
     [(NumC n) n]
     [(? BinOp? b) (do-binop b)]
-    ;[(PlusC l r) (+ (interp l) (interp r))]
-    ;[(MultC l r) (* (interp l) (interp r))]
-    ;[(SubC l r) (- (interp l) (interp r))]
-   ; [(DivC l r) (/ (interp l) (interp r))]
+    ;[(? Fcall? f) (function-lookup (Fcall-name f) )]
     [(IfLeq0? val then else) (cond [(<= (interp val) 0) (interp then)]
                                    [else (interp else)])]
     [other (error 'interp "QWJZ - unbound identifer")]))
-
-;; interp-fns , takes a list of fundefc's and calculates them
-
-(define (interp-fns [funs : (Listof FundefC)]) : Real
-  (match funs
-    [(cons a b) (if (= (FundefC-name a) 'main) 55 0)]))
 
 
 ;; do-binop : takes in a binop expression and returns a value
@@ -88,7 +83,61 @@
     [(BinOp '- l r) (- (interp l) (interp r))]
     [(BinOp '* l r) (* (interp l) (interp r))]
     [(BinOp '/ l r) (/ (interp l) (interp r))]
-    [other (error 'do-binop "QWJZ - unimplemented operand")]) )
+    #;[other (error 'do-binop "QWJZ - unimplemented operand")]) )
+
+
+;; interp-fns , takes a list of fundefc's and evaluetes them
+
+(define (interp-fns [funs : (Listof FundefC)]) : Real
+  (function-sub (function-lookup (Id 'main) funs) '() funs))
+
+
+;; function-lookup , given a name, the args, and a list of functions, find the function
+
+(define (function-lookup [name : Id] [funs : (Listof FundefC)] ) : FundefC
+  (match funs
+    [(cons a b) (if (equal? (FundefC-name a) name) a (function-lookup name b))]
+    [other (error 'function-lookup "QWJZ - function name ~e not found" (Id-id name))]))
+
+
+;; function-sub , given a list of args and expressions, sub in 
+;(define (function-sub))
+
+(define (function-sub [func : FundefC] [vars : (Listof Real)] [funs : (Listof FundefC)]) : Real
+  (match (FundefC-body func)
+    [(BinOp o l r) (interp (BinOp o
+                                  (NumC (function-sub (FundefC (FundefC-name func) (FundefC-args func) l) vars funs))
+                                  (NumC (function-sub (FundefC (FundefC-name func) (FundefC-args func) r) vars funs))))]
+    [(NumC n) (interp (NumC n))]
+    [(IfLeq0? v t e) (interp (IfLeq0?
+                              (NumC (function-sub (FundefC (FundefC-name func) (FundefC-args func) v) vars funs))
+                              (NumC (function-sub (FundefC (FundefC-name func) (FundefC-args func) t) vars funs))
+                              (NumC (function-sub (FundefC (FundefC-name func) (FundefC-args func) e) vars funs))))]
+    [(Fcall n args1) (if (arg-verify args1 (FundefC-args (function-lookup n funs)))
+                         (function-sub (function-lookup n funs) (arg-simplify args1 func vars funs) funs)
+                         (error 'function-sub "QWJZ - wrong number of arguments pushed"))]
+    [(Id s) (arg-find (Id s) vars (FundefC-args func)) ]))
+
+;; arg-simplify, given arguments and conditions of calling function, evaluate them until they become reals
+
+(define (arg-simplify [args : (Listof ExprC)] [func : FundefC] [vars : (Listof Real)] [funs : (Listof FundefC)]) : (Listof Real)
+  (match args
+    [(cons a b) (cons (function-sub (FundefC (FundefC-name func) (FundefC-args func) a) vars funs)
+                      (arg-simplify b func vars funs))]
+    ['() '()]))
+
+;; arg-verify , given function args and function call, check that the right amount of args were pushed
+
+(define (arg-verify [vars : (Listof ExprC)] [args : (Listof Id)]) : Boolean
+  (= (length vars) (length args)))
+
+;;arg-find , find the corresponding exprc to a symbol
+
+(define (arg-find [find : Id] [vars : (Listof Real)] [args : (Listof Id)]) : Real
+  (match* (vars args)
+    [('() '()) (error 'interp "QWJZ - unbound identifer") ]
+    [((cons f1 r1) (cons f2 r2)) (if (equal? find f2) f1 (arg-find find r1 r2))]))
+
 
 ;; Accepts an s-expression and calls the parser and then the interp function.
 (define (top-interp [exp : Sexp]) : Real
@@ -106,7 +155,7 @@
 (check-equal? (top-interp '{- 5 2}) 3)
 (check-exn #rx"QWJZ - expected valid expression"
            (lambda () (top-interp "fail")))
-(check-exn #rx"QWJZ - unimplemented operand"
+(check-exn #rx"QWJZ - unbound identifer"
            (lambda () (top-interp '{bruh 5 2})))
 (check-exn #rx"QWJZ - unbound identifer"
            (lambda () (top-interp '{fail})))
@@ -128,4 +177,24 @@
               (FundefC (Id 'foo) (list (Id 'x) (Id 'y)) (BinOp '+ (Id 'x) (Id 'y))))
 (check-exn #rx"QWJZ - wrong function definition format"
            (lambda () (parse-fundef '{hi})))
+
+
+;; testing for interp-fns
+(check-equal? (interp-fns
+       (parse-prog '{{f = {proc (x y) {+ x y}}}
+                     {main = {proc () {f 1 2}}}}))
+      3)
+
+
+(check-equal? (interp-fns
+        (parse-prog '{{f = {proc () 5}}
+                      {main = {proc () {+ {f} {f}}}}}))
+       10)
+
+
+(check-equal? (interp-fns
+        (parse-prog '{{f = {proc (x y z) {* {+ x y} z} }}
+                      {g = {proc (x z) {- z x}}}
+                      {main = {proc () {+ {f 3 2 4} {g 5 2}}}}}))
+       17)
 
